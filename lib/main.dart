cde76fe2c3f7e997f046822d0b3c5c259933ddb7
Kevin-Kiprotich <kevinkiprotich0089@gmail.com>
2025-03-12 12:28:03 +0300

    Initial Commit - First Production

---
+++ lib/main.dart
@@ -0,0 +1,123 @@
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // TRY THIS: Try running your application with "flutter run". You'll see
+        // the application has a purple toolbar. Then, without quitting the app,
+        // try changing the seedColor in the colorScheme below to Colors.green
+        // and then invoke "hot reload" (save your changes or press the "hot
+        // reload" button in a Flutter-supported IDE, or press "r" if you used
+        // the command line to start the app).
+        //
+        // Notice that the counter didn't reset back to zero; the application
+        // state is not lost during the reload. To reset the state, use hot
+        // restart instead.
+        //
+        // This works for code too, not just values: Most code changes can be
+        // tested with just a hot reload.
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  late WebViewController _webViewController;
+  bool _canGoBack = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _webViewController = WebViewController()
+      ..setJavaScriptMode(JavaScriptMode.unrestricted)
+      ..setUserAgent(
+          'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36')
+      ..setNavigationDelegate(NavigationDelegate(
+        onProgress: (progress) {},
+        onPageStarted: (url) {
+          _webViewController.canGoBack().then((bool canGoBack) {
+            print("Pop watch $_canGoBack");
+            setState(() {
+              _canGoBack = canGoBack;
+            });
+          });
+        },
+        onPageFinished: (url) {
+          _webViewController.canGoBack().then((bool canGoBack) {
+            print("Pop watch $_canGoBack");
+            setState(() {
+              _canGoBack = canGoBack;
+            });
+          });
+        },
+        onWebResourceError: (error) {
+          print(error);
+        },
+        onHttpError: (error) {
+          print(error);
+        },
+        onNavigationRequest: (request) {
+          return NavigationDecision.navigate;
+        },
+      ))
+      ..loadRequest(Uri.parse('https://courses.amryouthhub.org/'));
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return PopScope(
+      canPop: false,
+      onPopInvokedWithResult: (didPop, result) {
+        if (_canGoBack) {
+          _webViewController.goBack();
+        }
+      },
+      child: Scaffold(
+        body: SafeArea(
+          child: WebViewWidget(
+            controller: _webViewController,
+          ),
+        ),
+      ),
+    );
+  }
+}
